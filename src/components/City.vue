e5c6766bbb6a1e5fcf2e9870f0a6b0fc075c653d
kweku-ander <alexanderdarkoh8@gmail.com>
2024-08-12 03:18:54 +0000

    first commit on including vidoes and conditions in public folder

--- src/components/City.vue
+++ src/components/City.vue
@@ -3,6 +3,14 @@
     <span>{{ city.city }}</span>
     <div class="weather">
       <span>{{ Math.round(city.currentWeather.main.temp) }}&deg;</span>
+      <img :src="`../../public/conditions/${city.currentWeather.weather[0].icon}.svg`" alt="" />
+      <div class="video">
+        <video
+          autoplay
+          muted
+          :src="`../../public/conditions/${city.currentWeather.weather[0].icon}.mp4`"
+        ></video>
+      </div>
     </div>
   </div>
 </template>
