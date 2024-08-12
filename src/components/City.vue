c4b3edb3b2a5f3482db4b39d836a31eff69ef6d7
kweku-ander <alexanderdarkoh8@gmail.com>
2024-08-12 04:21:05 +0000

    commit on dynamically displaying video based on weather icon

--- src/components/City.vue
+++ src/components/City.vue
@@ -7,9 +7,11 @@
       <div class="video">
         <video
           autoplay
+          loop
           muted
-          :src="`../../public/conditions/${city.currentWeather.weather[0].icon}.mp4`"
+          :src="`../../public/videos/${city.currentWeather.weather[0].icon}.mp4`"
         ></video>
+        <div class="bg-overlay"></div>
       </div>
     </div>
   </div>
@@ -25,4 +27,67 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.city {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 20px;
+  flex-basis: 50%;
+  min-height: 250px;
+  color: white;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+
+  span {
+    z-index: 2;
+    text-transform: capitalize;
+    display: block;
+    font-size: 25px;
+    font-weight: 600;
+  }
+
+  .weather {
+    display: flex;
+    z-index: 1;
+    justify-content: flex-end;
+    align-items: flex-end;
+    flex: 1;
+
+    span {
+      font-size: 35px;
+      margin-right: 8px;
+    }
+
+    img {
+      height: 20px;
+      width: auto;
+      z-index: inherit;
+    }
+  }
+
+  .video {
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    widows: 100%;
+    height: 100%;
+
+    video {
+      height: 100%;
+      @media (min-width: 900px) {
+        height: auto;
+        width: 100%;
+      }
+    }
+
+    .bg-overlay {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+  }
+}
+</style>
